89b6a7ee24d3ff6c14ed4458580eeac1e738124d
krlim <regi3364@gmail.com>
2020-01-25 23:17:24 +0200

    25fin

--- activebox/app.js
+++ activebox/app.js
@@ -1,5 +1,25 @@
 $(function() {
-   console.log("Rabotaet!!!!!");
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+
+    $(window).on("scroll load resize", function(){
+       scrollPos = $(this).scrollTop();
+       introH = intro.innerHeight();
+       if(scrollPos > introH){
+           header.addClass("fixed");
+       } else{
+        header.removeClass("fixed");
+    }
+
+    });
+
+
+
+
+    header.addClass("fixed");
 
 
 
