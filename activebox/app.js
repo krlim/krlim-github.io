bc5c24eccd881d33b5d7400b4eda0c70adf02037
krlim <krlim.github.io>
2020-01-26 14:14:14 +0200

    kpkp

--- activebox/app.js
+++ activebox/app.js
@@ -3,27 +3,33 @@ $(function() {
     let intro = $("#intro");
     let introH = intro.innerHeight();
     let scrollPos = $(window).scrollTop();
-
+    checkScroll(scrollPos, introH);
 
     $(window).on("scroll load resize", function(){
        scrollPos = $(this).scrollTop();
        introH = intro.innerHeight();
-       if(scrollPos > introH){
-           header.addClass("fixed");
-       } else{
-        header.removeClass("fixed");
-    }
+       checkScroll(scrollPos, introH);
 
     });
 
-
-
-
+     function checkScroll(scrollPos, introH){
+        if(scrollPos > introH){
+           header.addClass("fixed");
+        } else{
+        header.removeClass("fixed");
+        }
+     }
     header.addClass("fixed");
 
+//    smooth scroll
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+        let elementID = $(this).data("scroll");
+        let elementOffset = $(elementID).offset().top;
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        },700);
 
-
-
-
+    });
 
 });
