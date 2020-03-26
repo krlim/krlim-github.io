edeab2ca9545dca13ad2a91b2ad5408f64f6f093
krlim <regi3364@gmail.com>
2020-03-26 20:53:43 +0200

    111

--- spa/app.js
+++ spa/app.js
@@ -4,20 +4,31 @@ $(function() {
     let intro = $("#intro");
     let scrollPos = $(window).scrollTop();
     let introH = intro.innerHeight();
+    checkScroll(scrollPos, introH);
     $(window).on("scroll load resize",function() {
         introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
+        checkScroll(scrollPos, introH);
+    });
 
+    function checkScroll(scrollPos, introH) {
         if(scrollPos > introH) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
-    });
+    }
+
 
     //**** Burger ****
     let burger = $("#burger");
     burger.on("click", function() {
         $(this).toggleClass("active");
     });
+
+
+
+
+
+
 });
