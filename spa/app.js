7602496e883fa8e6fe7cbfc86708be28d7414efe
Lancaster255 <regi3364@gmail.com>
2020-03-27 05:57:02 +0200

    js

--- spa/app.js
+++ spa/app.js
@@ -4,11 +4,15 @@ $(function() {
     let intro = $("#intro");
     let scrollPos = $(window).scrollTop();
     let introH = intro.innerHeight();
+    let burger = $("#burger");
+    let nav = $("#nav");
     checkScroll(scrollPos, introH);
     $(window).on("scroll load resize",function() {
         introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
         checkScroll(scrollPos, introH);
+        nav.removeClass("show");
+        burger.removeClass("active");
     });
 
     function checkScroll(scrollPos, introH) {
@@ -21,10 +25,27 @@ $(function() {
 
 
     //**** Burger ****
-    let burger = $("#burger");
+
     burger.on("click", function() {
+        event.preventDefault();
         $(this).toggleClass("active");
+        nav.toggleClass("show");
     });
+
+
+    //**** Nav ****
+
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+        let elementID = $(this).data("scroll");
+        let elementOffset = $(elementID).offset().top;
+        nav.removeClass("show");
+        $("html, body").animate({
+            scrollTop: elementOffset -70
+        },700);
+
+    });
+
 
 
 
