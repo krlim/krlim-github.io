0f11500b8a8ba97cdc34e488014a23ee2d24cb71
krlim <regi3364@gmail.com>
2020-04-29 22:28:17 +0300

    eeee

--- udemy/pr1/code.js
+++ udemy/pr1/code.js
@@ -1,2 +1,18 @@
-let money = prompt("Ваш бюджет на месяц?"," "),
-    time = prompt("Введите дату в формате YYYY-MM-DD","2020-04-22");
+let money = prompt("Ваш бюджет на месяц?",""),
+    time = prompt("Введите дату в формате YYYY-MM-DD","2020-04-22"),
+    appData = {
+        moneyData: money,
+        timeData: time,
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
+        savings: false,
+    };
+
+    for (let i = 0; i < 2; i++)
+    {
+        appData.expenses[prompt("Введите обязательную статью расходов в этом месяце", "")] = prompt("Во сколько обойдется?", "");
+    }
+
+    alert("Ваш бюджет на 1 день: " + appData.moneyData/30);
+
